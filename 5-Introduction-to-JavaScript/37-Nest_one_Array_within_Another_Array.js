8ef0108b339ba91e153866ee6da3224594fd926c
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 22:13:31 -0600

    Build a 2-dimensional array

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,7 @@
+// you can also build 2-dimensional arrays
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [[1,2,3], ["a", "array"]];

dec038bfc009ef25d704bbe2aa574f8be8acb3bb
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 20:40:40 -0600

    Defined variables in js

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,8 @@
+// seven data types in JS- undefined, null, boolean, string, symbol, number, and object //
+// variables store different types of data values //
+
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;

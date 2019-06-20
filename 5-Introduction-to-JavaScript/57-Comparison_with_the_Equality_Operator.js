448927767df1e4ab502eee4a7afdea576df48b37
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-20 13:16:40 -0600

    Added equality operator to test value

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,13 @@
+// Comparison operators return a boolean true or false
+// equality operator == helps us complete type conversion for js to compare differnt data types
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);

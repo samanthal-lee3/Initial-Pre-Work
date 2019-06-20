57201ba4e1ea4123ea07dbd205f031406b7b689a
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-20 13:11:29 -0600

    Changed function to return a different boolean value

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,12 @@
+// Booleans- may only be two values, true or false
+// true is on, false is off
+// boolean values are never written with quotes
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}

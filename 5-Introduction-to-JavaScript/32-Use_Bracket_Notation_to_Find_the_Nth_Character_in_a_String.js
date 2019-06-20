5e8868444c790da872ec4ddbd2056fb2d4ce90d0
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 21:59:57 -0600

    Used bracet notation to find second element in a string

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,12 @@
+// bracket notation can be used to get the character at other positions in a string
+// pass the desired index in the [] to find the value of that index
+
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];

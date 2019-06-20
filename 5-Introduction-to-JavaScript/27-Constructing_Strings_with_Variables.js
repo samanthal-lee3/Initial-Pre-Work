351485a56cc46cd6a1c9f202619c56db30659354
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 21:40:17 -0600

    Used + operator to add strings

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+// concatenation operator lets you insiert one or more variables into a string
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Samantha";
+var myStr = "My name is " +myName+ " and I am well!";

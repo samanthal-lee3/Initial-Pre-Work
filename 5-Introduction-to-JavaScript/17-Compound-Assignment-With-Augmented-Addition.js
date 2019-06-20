e8353b78facb343cba83ea7e91a67076326ad2bc
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 21:16:23 -0600

    Using += operator to shorten code and assignments to variables

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,14 @@
+// assignments can be used to modify the contents of a variable
+// everything to the right of an = is evaluated first
+// += operator adds a value to a variable
+// console.log returns the output of an operation or function
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;

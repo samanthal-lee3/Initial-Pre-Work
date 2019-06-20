41a767324a812a19dfcdacfbe94ece5547b19e88
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 22:11:15 -0600

    Applied += operator to conocate strings

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,14 @@
+// using + operator to add values into a string
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+
+  var result = "";
+  result += "My "+myNoun+" "+myAdjective+" "+myAdverb+" "+myVerb+".";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");

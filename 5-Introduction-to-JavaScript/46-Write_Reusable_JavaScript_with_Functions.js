3083a46bf0e84f22076d4a0ef028c01da025c001
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 22:31:43 -0600

    Wrote and called js function

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,15 @@
+// functions in js divide up code into reusable parts
+// call = invoke a function by using its name();
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+reusableFunction();

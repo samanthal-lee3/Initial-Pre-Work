c1f2259794116ce90fbf3d02d9bfddc0e8cd195b
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 21:44:28 -0600

    Calling index value on lastName string

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,16 @@
+// bracket notation is a way to get a character at a specific index in a string
+// index counting starts at 0, called zero-based indexing
+// you can call an index by [indexNum]
+
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];

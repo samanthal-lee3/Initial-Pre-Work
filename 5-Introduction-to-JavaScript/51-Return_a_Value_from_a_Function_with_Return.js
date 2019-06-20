5f3eb51a88091e014e2f7e0744e33cd840515739
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-20 12:59:45 -0600

    Returned value of a function

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,16 @@
+// pass values into a function with arguments
+// return statement will send back the value of a function
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+
+function timesFive(num) {
+  return num * 5;
+}
+
+console.log(minusSeven(10));
+console.log(timesFive(5));

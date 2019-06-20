ba5ac8ddba2f20ec77dbca299140533245b97d44
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 22:30:07 -0600

    Used .push method to add 5 arrays to an array

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,7 @@
+// array application
+var myList = [];
+myList.push(["apple", 2]);
+myList.push(["banana", 1]);
+myList.push(["cookies", 25]);
+myList.push(["steak", 4])
+myList.push(["water", 10]);

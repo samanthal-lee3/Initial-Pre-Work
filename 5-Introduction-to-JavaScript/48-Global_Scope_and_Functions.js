5f851fdca51d2e0fb7bb1499dfb9023a9eacc230
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 22:36:55 -0600

    Defined global variable

---
+++ 5-Introduction-to-JavaScript/48-Global_Scope_and_Functions.js
@@ -0,0 +1,23 @@
+// scope is the visibility of variables within code
+// variables defined outside a function are global- they can be seen everywhere
+// variables defined with var are global
+// Declare your variable here
+
+var myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}

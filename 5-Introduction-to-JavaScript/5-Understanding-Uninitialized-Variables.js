498b4f982904b189571b07a2b1b7b656d81184df
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 20:49:25 -0600

    Initialized variables in js

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,12 @@
+// variables declared in js have an initial value of undeclared  //
+// mathematical operation on undefined variable returned NaN //
+// Initialize these three variables
+var a =5;
+var b=10;
+var c="I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";

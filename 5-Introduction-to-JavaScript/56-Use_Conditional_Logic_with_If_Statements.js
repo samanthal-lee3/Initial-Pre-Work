01ecb0d07b0750148adc82150f3ff817cb7e460f
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-20 13:13:54 -0600

    Wrote if statement inside js function

---
+++ 5-Introduction-to-JavaScript/56-Use_Conditional_Logic_with_If_Statements.js
@@ -0,0 +1,27 @@
+// if statements make decision in code
+// keyword If tells js to execute code under certain conditions
+// conditions are boolean conditions and are only either true or false
+// Example
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+
+  // Only change code below this line.
+  if (wasThatTrue) {
+      return "Yes, that was true";
+  }
+    return "No, that was false";
+
+
+  // Only change code above this line.
+
+}
+
+// Change this value to test
+trueOrFalse(true);

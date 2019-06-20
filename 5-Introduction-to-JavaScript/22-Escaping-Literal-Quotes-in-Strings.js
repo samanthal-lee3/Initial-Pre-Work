53fc0e0fda3223060651bf8c58e6ab1762a39553
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 21:28:01 -0600

    Escaped quotes within a string

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,3 @@
+// defined strings start and end with single or double quote
+// escape quotes within a string by using \
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line

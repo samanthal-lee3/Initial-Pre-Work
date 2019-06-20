6a090da49ffe8f8129d743401ff6e64892bf2ecf
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 21:58:43 -0600

    Reset immutable string variable

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,9 @@
+// string values are immutable
+// they cannont be altereted once created
+// variables can be changed but the individual characters of the string cannot be altereted
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me

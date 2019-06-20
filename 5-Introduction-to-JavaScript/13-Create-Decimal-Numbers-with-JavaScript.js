99508581d6948b9b60ac15f680174b581c405886
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 21:09:22 -0600

    Built up float data type

---
+++ 5-Introduction-to-JavaScript/13-Create-Decimal-Numbers-with-JavaScript.js
@@ -0,0 +1,8 @@
+// decimal numbers can be stored in variables
+// floating point or float numbers mean a number with a decimal
+
+var ourDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal = 5.7;

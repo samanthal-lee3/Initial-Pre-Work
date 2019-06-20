fd35607a9f4ebcad0dc9e0ba5f6bf158d789cd33
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-20 13:10:08 -0600

    Edited nextInLine function- add an item to the array, remove a specific index from the array

---
+++ 5-Introduction-to-JavaScript/54-Stand_in_Line.js
@@ -0,0 +1,19 @@
+// queue- an abstract Data Structure where items are kept in order
+// new items can be added at the back of the queue
+// old items are taken off from the front of the queue
+
+function nextInLine(arr, item) {
+  // Your code here
+    arr.push(item);
+    var removed = arr.shift();
+    return removed;
+  }
+
+
+// Test Setup
+var testArr = [1,2,3,4,5];
+
+// Display Code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log("After: " + JSON.stringify(testArr));

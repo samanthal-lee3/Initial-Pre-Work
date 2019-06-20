8422bdd35cfb3c31cdc3d35a8da7c63adeb9acbb
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-20 13:01:59 -0600

    Returnin an undefined value

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,18 @@
+// functions do not have to contain the return statement
+// without a return statement, the returned value is undefined
+
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive() {
+  sum = sum +5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();

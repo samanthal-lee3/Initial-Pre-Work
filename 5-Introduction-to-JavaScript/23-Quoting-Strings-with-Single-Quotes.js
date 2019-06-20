8a32c0c1e1a88c3047a1ab0e8bcae16d4b96cd9b
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 21:30:09 -0600

    Using single and double quotes in a string

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,4 @@
+// single and double quotes work the same in js
+// single and double quotes can be used with a string instead of using the \
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';

3abe46958bf85758400517b364f96c2b32047edb
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 22:02:44 -0600

    Used .length to find certain value in a string

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+// .length-n can be used to find any element in a string
+
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length-2];

17adc01b939123cd068068b2928c607e53f3dd6b
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-20 13:38:28 -0600

    Applied logical operator

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,17 @@
+// && logical and operator
+// returns true if the operands on the left and right are true
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >=25) {
+      return "Yes";
+    }
+
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(10);

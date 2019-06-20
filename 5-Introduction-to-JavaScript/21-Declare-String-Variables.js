7ba49f82dd33486ff9730efd33ea484fc113f20f
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 21:25:38 -0600

    Built two variables of string literals

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,11 @@
+// string literal is a series of zero or more characters enclosed in single or double quotes
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+
+var myFirstName = "Samantha";
+var myLastName = "Lee";

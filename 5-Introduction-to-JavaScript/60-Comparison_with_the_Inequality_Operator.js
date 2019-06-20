041a9f12eaa581a954bd78339fc0a5af7f8148a8
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-20 13:26:32 -0600

    Applied inequality operator to function

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,13 @@
+// != is called the inequality operator
+// means not equal, returns false if given data is inequality
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);

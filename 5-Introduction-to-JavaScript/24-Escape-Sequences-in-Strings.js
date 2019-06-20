e488831d78a03af97121adadd1a1d7521120183f
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 21:35:32 -0600

    Practiced with different escaped characters

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,4 @@
+// reason 1 for escaping characters: use characters you might not otherwise be able to type out, like a backspace
+// reason 2 for escaping characters: represent multiple quotes in a string
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line

7606e221757224841e2afead0324d72af02eaf7b
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-20 13:40:23 -0600

    Applied or operator

---
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -0,0 +1,17 @@
+// logical or operator ||
+// returns true if either of the operands is true
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val > 20 || val < 10) {
+    return "Outside";
+  }
+
+
+  // Only change code above this line
+  return "Inside";
+}
+
+// Change this value to test
+testLogicalOr(15);

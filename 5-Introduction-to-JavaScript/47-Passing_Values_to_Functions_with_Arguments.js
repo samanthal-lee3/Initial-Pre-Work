a1429f42c7a0ad79a2edaa87a40a76a5cbe08e1c
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 22:33:16 -0600

    Built function to take 2 parameters

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,16 @@
+// parameters are variable that act as place holders for values being put into a function
+// we define functions with parameters
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+
+function functionWithArgs(a, b){
+  console.log(a + b);
+}
+
+functionWithArgs(2, 4);

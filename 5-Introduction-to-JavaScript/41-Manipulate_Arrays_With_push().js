3b3b94a5914d32f221def1c1800dbece731ddadf
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 22:24:01 -0600

    Pushed new array on myArray

---
+++ 5-Introduction-to-JavaScript/41-Manipulate_Arrays_With_push().js
@@ -0,0 +1,13 @@
+// push method allows you to add new items on an array
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+
+myArray.push(["dog", 3]);

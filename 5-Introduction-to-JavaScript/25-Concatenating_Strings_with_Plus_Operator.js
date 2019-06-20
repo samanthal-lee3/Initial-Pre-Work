afbe0c7f6030a61439c5784febbc123ba25f9c09
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-19 21:37:08 -0600

    Concatenated two strings with + operator

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,10 @@
+// + used with a string is called the concatenation operator
+// you can build a new string out of other strings by concatenating them together
+// concatenation does not add spaces between strings
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";

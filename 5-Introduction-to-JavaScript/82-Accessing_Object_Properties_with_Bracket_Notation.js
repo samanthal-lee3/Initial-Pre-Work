58f107e8b4fb9a49ccb968bbdf269c66369c5b3c
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 17:00:04 -0600

    Used bracket notation to pull properties with spaces in the name

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,13 @@
+// bracket notation lets you access properties with a space in the name
+
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line

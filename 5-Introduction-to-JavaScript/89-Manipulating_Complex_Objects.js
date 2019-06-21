73d46afb92774b5a7ce77190bf38d78f67a642c8
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 17:49:01 -0600

    Built an object in myMusic variable

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,29 @@
+// js objects can handle flexible data
+// js objects can hold multiple types of data, and have subsets of data within the objects
+// objects hold data in a property
+// JSON- js object notation, related data interchange format used to store data
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Beyonce",
+    "title": "Lemonade",
+    "release_year": 2017,
+    "formats": [
+      "CD",
+      "mp#"
+    ],
+    "gold": true
+  }
+  // Add record here
+];

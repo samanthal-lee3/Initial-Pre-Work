04d2e706d35a1a175ebeb297e913141ca8e475ef
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 11:19:42 -0600

    Chained long if/else statement

---
+++ 5-Introduction-to-JavaScript/71-Chaining_If_Else_Statements.js
@@ -0,0 +1,22 @@
+// if/else statements can be chained together
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else{
+    return "Huge";
+  // Only change code above this line
+}
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+// Change this value to test
+testSize(7);

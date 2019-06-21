246fe8502f4ad8d543e529c072401c5a6203870d
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 16:58:20 -0600

    Applied dot notation to locate a specific property of an object

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,15 @@
+// dot notation and bracket notation allow you to locate properties of an object
+// dot notation is used when you know the name of a property you are trying to access
+
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line

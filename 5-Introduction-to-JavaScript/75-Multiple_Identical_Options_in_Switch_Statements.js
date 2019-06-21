e5875ae62c3d033d00a7dde7c90f9a8d21668024
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 11:42:10 -0600

    Combined case statements before breaks

--- 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
+++ 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
@@ -1,1 +1,31 @@
+// without a break statement, the switch statement will continue until it reaches a break
+// multiple inputs with the same output can be combined before 1 break statement
 
+
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+
+  switch(val) {
+    case 1:
+    case 2:
+    case 3:
+      return "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      return "Mid";
+    case 7:
+    case 8:
+    case 9:
+      return "High";
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+sequentialSizes(1);

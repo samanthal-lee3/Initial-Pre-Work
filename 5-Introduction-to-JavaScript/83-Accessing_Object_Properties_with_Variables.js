10cd435b865499430852411293e54391c9f5c4e2
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 17:03:10 -0600

    Bracket notation to store property in a variable

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,13 @@
+// bracket notation can access a property stored as the value of a variable
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber=16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line

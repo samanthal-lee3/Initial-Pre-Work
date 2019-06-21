b94ccd7f56f50bb835e1a6798db69e61c82aa493
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 11:55:48 -0600

    Built instance of a js object

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,21 @@
+// objects are data sets that have properties
+// objects store data in a structured way
+// objects can represent real world objects
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "bob",
+  "legs": 5,
+  "tails": 1,
+  "friends": ["lots!"]
+
+};

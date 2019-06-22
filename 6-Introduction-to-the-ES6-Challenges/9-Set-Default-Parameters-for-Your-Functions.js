af03f3e7505516355df87465f30ed36da94e6418
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 20:31:33 -0600

    Using default function parameters

---
+++ 6-Introduction-to-the-ES6-Challenges/9-Set-Default-Parameters-for-Your-Functions.js
@@ -0,0 +1,10 @@
+// default parameters for functions let us create more flexible functions
+
+const increment = (function() {
+  "use strict";
+  return function increment(number, value = 1) {
+    return number + value;
+  };
+})();
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns 6

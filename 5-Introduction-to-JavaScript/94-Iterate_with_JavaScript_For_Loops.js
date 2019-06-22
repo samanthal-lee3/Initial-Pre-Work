52490e60df7a1cd54b2c02d08889d8806145b591
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 18:44:10 -0600

    Used for loop to build an array

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,21 @@
+// for loops runs for a specific number of times
+// declared with 3 optional expressions- initialization, condition, final-expression
+// initialization- executed one time before the loop starts; defines variable
+// condition- evaluated at the beginning of every loop and will continue as long as it is true
+// final-expression is executed at end of loop iteration, prior to the next condition
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for (var i = 1; i < 6; i++) {
+    myArray.push(i);
+}

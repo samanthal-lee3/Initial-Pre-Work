6b967746e89ce8704d156867030c4aa771d96f2c
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 19:22:27 -0600

    Called Math.random() js function

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,10 @@
+// math.random js function generates random decimal number between 0 and 1
+
+function randomFraction() {
+
+  // Only change code below this line.
+
+    return Math.random();
+
+  // Only change code above this line.
+}

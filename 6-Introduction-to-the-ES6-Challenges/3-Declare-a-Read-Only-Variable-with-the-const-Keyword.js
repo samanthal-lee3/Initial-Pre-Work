2676e0f4369efd0d03198b39c0e585ddca6a031e
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 19:59:46 -0600

    Built const read only variable

---
+++ 6-Introduction-to-the-ES6-Challenges/3-Declare-a-Read-Only-Variable-with-the-const-Keyword.js
@@ -0,0 +1,17 @@
+// const declares variables as read-only
+// you cannot reassign variables to const variables
+
+function printManyTimes(str) {
+  "use strict";
+
+  // change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // change code above this line
+
+}
+printManyTimes("freeCodeCamp");

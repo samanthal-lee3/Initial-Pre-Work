c2ade56a80651bd11511f7e4282fead1c3a8787d
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 18:46:44 -0600

    Build for loop to interate over odd numbers

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,18 @@
+// final-expression can determine on which conditions a for loop will trigger
+// the for loop does not have to happen every time, for example we can set to only happen every even time
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for (var i = 1; i <=9; i +=2) {
+  myArray.push(i);
+}

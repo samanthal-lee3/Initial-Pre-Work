2015280cf9e3b1bf48b37089d5f6a7b4a391e857
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 21:22:43 -0600

    Removed function keyword

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,15 @@
+// in ES5, use function keyword to define functions defining functions within objects
+// in ES^, you can remove function keyword and colon together
+
+
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);

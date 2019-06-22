aa281c00e8f004654359f184569c348b973c1b3f
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 20:48:03 -0600

    Extracted element of arr1

---
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,9 @@
+// spread operator expands arrays and other expressions when multiple parameters are expected
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(arr2);

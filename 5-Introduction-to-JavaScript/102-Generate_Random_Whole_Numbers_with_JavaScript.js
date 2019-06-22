b83b3d4a446397720b56d4efe886abdaa2f9a861
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 19:24:25 -0600

    Combined functions to generate random whole number

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,10 @@
+// generate random whole numbers using Math.random and Math.floor, which rounds number down to the nearest whole numbers
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10);
+}

4a77735beb8b1f0abb38d16f9ac67fe293697569
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 18:35:31 -0600

    Built while loop for specific condition

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,11 @@
+// iterate- run the same code multiple times using a loop
+// while loops- run while a specific condition is true and stops when the condition is no longer true
+
+// Setup
+var myArray = [];
+// Only change code below this line.
+var i = 0;
+while (i <= 4) {
+  myArray.push(i);
+  i++;
+}

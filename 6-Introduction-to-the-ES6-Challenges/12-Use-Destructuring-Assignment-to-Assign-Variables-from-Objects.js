1aaf86b0f6a77b487d9595d2b54871224660b67d
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 20:54:44 -0600

    Deconstructing assignments

---
+++ 6-Introduction-to-the-ES6-Challenges/12-Use-Destructuring-Assignment-to-Assign-Variables-from-Objects.js
@@ -0,0 +1,17 @@
+// spread operator can effectively unpack the contents of the array
+// destructing assignment is special syntax for neatly assigning values take from an object to a variable
+
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+  // change code below this line
+  const {tomorrow: tempOfTomorrow} = avgTemperatures; // change this line
+  // change code above this line
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79

c2df028b3a45fb77774cb034e3fbbc8f43c542c0
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 21:32:47 -0600

    Applied get and set methods

---
+++ 6-Introduction-to-the-ES6-Challenges/21-Use-getters-and-setters-to-Control-Access-to-an-Object.js
@@ -0,0 +1,27 @@
+// getters and setters
+// getter function return value of an objects private variable
+// setter modify the value
+
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+  class Thermostat{
+    constructor(farenheit){
+      this.farenheit = farenheit;
+    }
+    get temperature(){
+      return 5/9 *(this.farenheit - 32);
+    }
+    set temperature(celsius){
+      this.farenheit = celsius * 9.0/5+32;
+    }
+  }
+
+  /* Alter code above this line */
+  return Thermostat;
+}
+const Thermostat = makeClass();
+const thermos = new Thermostat(76); // setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in C
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in C

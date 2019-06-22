991d71997b9fc1f438fc226edab625a017ee980b
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 21:35:51 -0600

    Imported single element of code file

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,6 @@
+// require function used to import code in external files
+// import- choose individual pieces of code to load, rather than loading an entire piece of code
+
+"use strict";
+import {capitalizeString} from "string_functions"
+capitalizeString("hello!");

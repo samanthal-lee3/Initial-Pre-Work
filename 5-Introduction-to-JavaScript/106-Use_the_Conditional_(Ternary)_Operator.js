f9c55b3aeb1b600ce41f5fd9d560750ee1ba30b9
samanthal-lee3 <samanthal.lee3@gmail.com>
2019-06-21 19:33:06 -0600

    Built simplified if else statement with ternary operator

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,9 @@
+// conditional operator- ternary operator
+// uses as one line if-else statement
+// condition ? statement-if-true : statement-if-false
+
+function checkEqual(a, b) {
+  return (a === b ? true : false);
+}
+
+checkEqual(1, 2);
